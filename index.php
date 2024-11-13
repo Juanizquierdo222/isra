6616b5c047a519f8db529cc79fa9024f58c2d6e6
Izquierdo <101188134+Juanizquierdo222@users.noreply.github.com>
2024-11-13 16:07:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,452 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>peliculas</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+
+    
+    
+    <header class="header">
+        
+
+        <div class="menu container">
+            <a href="#" class="logo" >cineTec</a>
+            <input type="checkbox" id="menu" />
+            <label for="menu">
+                <img src="imagenes/menu.png" class="menu-icono" alt="menu">
+            </label>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Genero</a></li>
+                    <li><a href="#">Peliculas</a></li>
+                    <li><a href="#">Populares</a></li>
+                </ul>
+            </nav>
+            <a href="#formulario" class="btn-1">Registrate</a>
+
+            
+        </div>
+
+        <div class="header-content-container">
+
+            <div class="header-1">
+                <img src="imagenes/venom.png" alt="">
+                <a href="#" class="btn-2">Ver ahora</a>
+                <div class="header-2">
+                    <h1>Las mejores <br> peliculas</h1>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="movies container">
+
+        <h2>Populares</h2>
+        <hr>
+        <div class="box-container-1">
+
+            <div class="box-1">
+                <div class="content">
+                    
+                    <img src="imagenes/1.jpg" alt="">
+                    
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        En "Iron Man 3" de Marvel, el descarado pero brillante empresario Tony Stark/Iron Man se enfrentará a un enemigo cuyo poder no conoce límites. Cuando Stark comprende que su enemigo ha destruido su universo personal, se embarca en una angustiosa búsqueda para encontrar a los responsables. Este viaje pondrá a prueba su entereza una y otra vez.
+                    </p>
+                    
+
+                </div>
+            </div>
+
+            <div class="box-1">
+                <div class="content">
+                    <img src="imagenes/2.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Mucho tiempo después de los eventos sucedidos en Avengers: Endgame, los Guardianes de la Galaxia deben convencer a Thor para que regrese al campo de batalla y se enfrente a un villano empeñado en matar a todas las criaturas divinas en todo el cosmos.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-1">
+                <div class="content">
+                    <img src="imagenes/3.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Thor está preso al otro lado del universo sin su poderoso martillo y se enfrenta a una carrera contra el tiempo. Su objetivo es volver a Asgard y parar el Ragnarok porque significaría la destrucción de su planeta natal y el fin de la civilización Asgardiana a manos de una todopoderosa y nueva amenaza, la implacable Hela.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-1">
+                <div class="content">
+                    <img src="imagenes/4.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-1">
+                <div class="content">
+                    <img src="imagenes/5.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-1">
+                <div class="content">
+                    <img src="imagenes/6.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-1">
+                <div class="content">
+                    <img src="imagenes/7.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-1">
+                <div class="content">
+                    <img src="imagenes/8.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="load-more" id="load-more-1">Cargar mas</div>
+
+    </section>
+
+    <section class="movies container">
+
+        <h2>Peliculas de accion</h2>
+        <hr>
+        <div class="box-container-2">
+
+            <div class="box-1">
+                <div class="content">
+                    <img src="imagenes/9.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-2">
+                <div class="content">
+                    <img src="imagenes/10.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-2">
+                <div class="content">
+                    <img src="imagenes/11.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-2">
+                <div class="content">
+                    <img src="imagenes/12.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-2">
+                <div class="content">
+                    <img src="imagenes/13.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-2">
+                <div class="content">
+                    <img src="imagenes/14.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-2">
+                <div class="content">
+                    <img src="imagenes/15.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-2">
+                <div class="content">
+                    <img src="imagenes/16.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="load-more" id="load-more-2">Cargar mas</div>
+
+    </section>
+
+    <section class="movies container">
+
+        <h2>Peliculas de Estreno</h2>
+        <hr>
+        <div class="box-container-3">
+
+            <div class="box-3">
+                <div class="content">
+                    <img src="imagenes/25.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-3">
+                <div class="content">
+                    <img src="imagenes/26.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Eddie y Venom están a la fuga. Perseguidos por sus sendos mundos y cada vez más cercados, el dúo se ve abocado a tomar una decisión devastadora que hará que caiga el telón sobre el último baile de Venom y Eddie.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-3">
+                <div class="content">
+                    <img src="imagenes/19.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-3">
+                <div class="content">
+                    <img src="imagenes/20.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-3">
+                <div class="content">
+                    <img src="imagenes/21.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-3">
+                <div class="content">
+                    <img src="imagenes/22.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-3">
+                <div class="content">
+                    <img src="imagenes/23.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="box-3">
+                <div class="content">
+                    <img src="imagenes/24.jpg" alt="">
+                    <h3>Pelicula hd</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nobis similique sint ab voluptatum deserunt, perspiciatis 
+                        oluptate voluptates illo tenetur? Optio libero numquam ex.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="load-more" id="load-more-3">Cargar mas</div>
+
+    </section>
+
+
+    <a href="#formulario" class="btn-1">Registrate</a>
+
+    <section id="formulario" class="formulario-container">
+    <form method="POST" action="send.php" autocomplete="off" onsubmit="return myFunction()">
+        <h2>Regístrate y Crea tu Perfil</h2>
+        
+
+        <div class="input-group">
+            <div class="input-container">
+                <input type="text" name="phone" placeholder="Teléfono" required>
+                <i class="fas fa-phone"></i>
+            </div>
+
+            <div class="input-container">
+                <input type="email" name="email" placeholder="Correo" required>
+                <i class="fas fa-envelope"></i>
+            </div>
+
+            <div class="input-container">
+                <input type="password" name="password" placeholder="Contraseña" required>
+                <i class="fas fa-lock"></i>
+            </div>
+
+            <div class="input-container">
+                <input type="text" name="nombre" placeholder="Nombre" required>
+            </div>
+
+            <div class="input-container">
+                <input type="text" name="apellido" placeholder="Apellido" required>
+            </div>
+
+            <button type="submit" name="send" class="btn">Enviar</button>
+        </div>
+    </form>
+</section>
+
+
+
+
+
+
+
+
+
+    <footer class="footer container">
+        <h3>cinemeTec</h3>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="https://www.facebook.com/kxrmaeubloodformercy/" target="_blank" rel="noopener noreferrer">Nosotros</a></li>
+
+            <li><a href="#">Peliculas</a></li>
+            <li><a href="#">Contactos</a></li>
+        </ul>
+        <p>Copyright © 2024 - cinemeTec. 
+            Este es el mejor catálogo de medios de entretenimiento que cubre películas. No pierdas tu tiempo libre y relájate con cinemeTec.</p>
+    </footer>
+
+    <?php
+    include("send.php");
+    ?>
+    <script>
+        function myFunction() {
+            window.location.href = "http://localhost/paginapelis";
+        
+        }
+    </script>
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,49 @@
+let currentItem1 = 4; 
+let currentItem2 = 4; 
+let currentItem3 = 4; 
+
+// Función para cargar más películas en la sección 1
+document.querySelector('#load-more-1').onclick = () => {
+    let boxes1 = [...document.querySelectorAll('.box-container-1 .box-1')];
+    for (let i = currentItem1; i < currentItem1 + 4; i++) {
+        if (i < boxes1.length) {
+            boxes1[i].style.display = 'block'; 
+        }
+    }
+    currentItem1 += 4;
+    if (currentItem1 >= boxes1.length) {
+        document.querySelector('#load-more-1').style.display = 'none'; sz
+    }
+    
+
+};
+
+
+
+// Función para cargar más películas en la sección 2
+document.querySelector('#load-more-2').onclick = () => {
+    let boxes2 = [...document.querySelectorAll('.box-container-2 .box-2')];
+    for (let i = currentItem2; i < currentItem2 + 4; i++) {
+        if (i < boxes2.length) {
+            boxes2[i].style.display = 'block'; 
+        }
+    }
+    currentItem2 += 4;
+    if (currentItem2 >= boxes2.length) {
+        document.querySelector('#load-more-2').style.display = 'none'; 
+    }
+};
+
+// Función para cargar más películas en la sección 3
+document.querySelector('#load-more-3').onclick = () => {
+    let boxes3 = [...document.querySelectorAll('.box-container-3 .box-3')];
+    for (let i = currentItem3; i < currentItem3 + 4; i++) {
+        if (i < boxes3.length) {
+            boxes3[i].style.display = 'block'; 
+        }
+    }
+    currentItem3 += 4;
+    if (currentItem3 >= boxes3.length) {
+        document.querySelector('#load-more-3').style.display = 'none'; 
+    }
+};
