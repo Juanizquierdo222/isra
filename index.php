4a09b638f79b23920b7f81f4a8742cc7197fcbd8
Juanizquierdo222 <juaneslaonda66@gmail.com>
2024-11-22 11:51:46 -0500

    seo

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>peliculas</title>
-    <meta name="description" content="Disfruta de las mejores películas, incluyendo estrenos, películas de acción, y más en cineTecc. Regístrate y accede a un catálogo exclusivo de películas en alta definición.">
-    <meta name="keywords" content="películas, cine, estrenos, películas de acción, cine online, cineTecc, ver películas, streaming de películas">
+    <meta name="description" content=" reseña de peliculas y ver trailers y más en cineTecc.">
+    <meta name="keywords" content="películas, cine, estrenos, películas de acción, cineTecc">
     <meta name="author" content="cineTecc">
     <meta property="og:title" content="cineTecc - Las mejores películas en un solo lugar">
     <meta property="og:description" content="Explora una amplia colección de películas populares y de estreno. Únete a cineTecc y disfruta del mejor contenido.">
